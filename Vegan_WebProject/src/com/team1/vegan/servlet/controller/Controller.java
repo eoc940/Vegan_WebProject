8a669f59da0871da98ace61fb0a92874c12a6172
Koartifact <k7and3@gmail.com>
2021-04-30 14:03:23 +0900

    new

--- Vegan_WebProject/src/com/team1/vegan/servlet/controller/Controller.java
+++
@@ -1,8 +0,0 @@
-package com.team1.vegan.servlet.controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Controller {
-	ModelAndView execute(HttpServletRequest request, HttpServletResponse response);
-}
